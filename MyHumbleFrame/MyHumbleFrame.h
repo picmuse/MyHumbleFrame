338740744921b3953dc7bcb6c7704b7867623bc5
Dongseop Lim <picmuse@Dongseops-MacBook-Pro.local>
2017-03-29 20:03:11 +0900

    Initial Commit

---
+++ MyHumbleFrame/MyHumbleFrame.h
@@ -0,0 +1,19 @@
+//
+//  MyHumbleFrame.h
+//  MyHumbleFrame
+//
+//  Created by Dongseop Lim on 29/03/2017.
+//  Copyright © 2017 Dongseop Lim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyHumbleFrame.
+FOUNDATION_EXPORT double MyHumbleFrameVersionNumber;
+
+//! Project version string for MyHumbleFrame.
+FOUNDATION_EXPORT const unsigned char MyHumbleFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyHumbleFrame/PublicHeader.h>
+
+
